215b01744c728f4db195803081db0fbe5871dd4b
MowMowchow <darkstar.hou2@gmail.com>
2023-01-22 13:14:54 -0500

    model update

--- internal/models/c1Rewards.go
+++ internal/models/c1Rewards.go
@@ -1,16 +1,31 @@
 package models
 
+type Transaction struct {
+	Date         string `json:"date,omitempty"`
+	MerchantCode string `json:"merchant_code,omitempty"`
+	AmountCents  int    `json:"amount_cents,omitempty"`
+}
+
 type TransactionsSummary struct {
 	Merchants map[string]int // Total amount spent at each merchant
 	MaxCents  int
 }
 
-type Transaction struct {
-	Date         string `json:"date,omitempty"`
-	MerchantCode string `json:"merchant_code,omitempty"`
-	AmountCents  int    `json:"amount_cents"`
+type RewardMerchant struct {
+	Cost   int `json:"cost,omitempty"`
+	Points int `json:"points,omitempty"`
+}
+
+type RewardRule struct {
+	Merchants map[string]RewardMerchant `json:"merchants,omitempty"`
+}
+
+type Rules struct {
+	Rules  []RewardRule `json:"rules,omitempty"`
+	Length int
 }
 
 type CalculateTransactionRequest struct {
-	Transactions map[string]Transaction `json:"transactions"`
+	Transactions map[string]Transaction `json:"transactions,omitempty"`
+	Rules        []RewardRule           `json:"rules,omitempty"`
 }
