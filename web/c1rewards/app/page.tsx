ab13864ceb2dffbfb182be2fe1c62e9e02e4bf46
MowMowchow <darkstar.hou2@gmail.com>
2023-01-23 17:06:39 -0500

    ui_update

--- web/c1rewards/app/page.tsx
+++ web/c1rewards/app/page.tsx
@@ -1,7 +1,4 @@
 "use client";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
 import { useState } from "react";
 import {
   MaximumRewardsResponseBody,
@@ -16,8 +13,7 @@ import RuleMerchantRow from "./components/RuleMerchantRow";
 import { fetchMaximumRewards } from "@/internal/services/rewards";
 import { formatMaximumRewardsRequest } from "@/internal/utils";
 import defaultValues from "./defaultValues.json";
-import { stringify } from "querystring";
-import TransactionGroupRow from "./components/TransactionGroup";
+import TransactionGroupRow from "./components/TransactionGroupRow";
 
 export default function Home() {
   // const [transactionList, setTransactionList] = useState<Transaction[]>([]);
@@ -152,23 +148,23 @@ export default function Home() {
   };
 
   return (
-    <div className="grid gird-row-1 justify-center w-11/12 md:w-11/12 lg:w-9/12 ">
-      <div className="grid gird-cols-1">
+    <div className="flex flex-row justify-center">
+      <div className="grid gird-cols-1 w-11/12 md:w-11/12 lg:w-9/12">
         <div className="px-5 font-poppins">
           <div className="grid grid-cols-1">
             <div className="m-0 sm:m-4 md:m-10" />
             <div className="flex flex-row justify-center">
               <div className="flex flex-col justify-around">
-                <h1 className="text-3xl">C1 Rewards Calculator</h1>
-                <h3 className="text-xl">
-                  by Jason Hou (jasonhou0299@gmail.com)
+                <h1 className="text-5xl">C1 Rewards Calculator</h1>
+                <h3 className="ml-2 text-lg">
+                  - by Jason Hou (jasonhou0299@gmail.com)
                 </h3>
               </div>
             </div>
             {/* Transactions */}
             <div className="m-0 sm:m-4 md:m-10" />
             <div className="">
-              <h1 className="text-2xl">Add New Transaction</h1>
+              <h1 className="text-3xl">Transactions</h1>
               <div className="mt-4 flex flex-row justify-center">
                 <form
                   onSubmit={(event) => {
@@ -177,7 +173,7 @@ export default function Home() {
                   }}
                   className="w-full"
                 >
-                  <div className="flex flex-row justify-around">
+                  <div className="flex flex-row justify-around flex-wrap">
                     <input
                       title="name"
                       type="text"
@@ -188,8 +184,8 @@ export default function Home() {
                           event.target.value
                         );
                       }}
-                      placeholder="name"
-                      className="border-2 border-stone-400 rounded-md"
+                      placeholder=" name"
+                      className="m-2 border-2 border-stone-400 rounded-md"
                     />
                     <input
                       title="date"
@@ -201,8 +197,8 @@ export default function Home() {
                           event.target.value
                         );
                       }}
-                      placeholder="date"
-                      className="border-2 border-stone-400 rounded-md"
+                      placeholder=" date"
+                      className="m-2 border-2 border-stone-400 rounded-md"
                     />
                     <input
                       title="merchant_code"
@@ -214,8 +210,8 @@ export default function Home() {
                           event.target.value
                         );
                       }}
-                      placeholder="merchant_code"
-                      className="border-2 border-stone-400 rounded-md"
+                      placeholder=" merchant_code"
+                      className="m-2 border-2 border-stone-400 rounded-md"
                     />
                     <input
                       title="amount_cents"
@@ -227,14 +223,14 @@ export default function Home() {
                           event.target.value
                         );
                       }}
-                      placeholder="amount_cents"
-                      className="border-2 border-stone-400 rounded-md"
+                      placeholder=" amount_cents"
+                      className="m-2 border-2 border-stone-400 rounded-md"
                     />
                     <button
-                      className="py-1 px-2 rounded-md bg-emerald-400"
+                      className="py-1 px-2 shadow-md rounded-md bg-cyan-500"
                       type="submit"
                     >
-                      Add Transaction
+                      <h1 className="text-lg text-stone-50">Add Transaction</h1>
                     </button>
                   </div>
                 </form>
@@ -248,7 +244,6 @@ export default function Home() {
                       date={transaction.date}
                       merchant_code={transaction.merchant_code}
                       amount_cents={transaction.amount_cents}
-                      max_rewards={-2}
                       deleteTransaction={handleDeleteTransaction}
                     />
                   ))}
@@ -257,8 +252,8 @@ export default function Home() {
             </div>
 
             {/* Rules */}
-            <div className="flex flex-col justify-center">
-              <h1 className="text-2xl">Add New Rules</h1>
+            <div className="mt-8 flex flex-col justify-center">
+              <h1 className="text-3xl">Add New Rules</h1>
               <p>
                 *NOTE: add a merchant name and cost for the rule, and then add
                 the rule
@@ -271,7 +266,7 @@ export default function Home() {
                   }}
                   className="w-full"
                 >
-                  <div className="flex flex-row justify-around">
+                  <div className="flex flex-row justify-around flex-wrap">
                     <input
                       title="name"
                       type="text"
@@ -282,7 +277,7 @@ export default function Home() {
                           event.target.value
                         );
                       }}
-                      placeholder="name"
+                      placeholder=" name"
                       className="border-2 border-stone-400 rounded-md"
                     />
                     <input
@@ -295,15 +290,15 @@ export default function Home() {
                           event.target.value
                         );
                       }}
-                      placeholder="points"
+                      placeholder=" points"
                       className="border-2 border-stone-400 rounded-md"
                     />
                     <div className="flex flex-row justify-around">
                       <button
-                        className="py-1 px-2 rounded-md bg-emerald-400"
+                        className="py-1 px-2 shadow-md rounded-md bg-cyan-500"
                         type="submit"
                       >
-                        Add Rule
+                        <h1 className="text-lg text-stone-50">Add Rule</h1>
                       </button>
                     </div>
                   </div>
@@ -311,6 +306,9 @@ export default function Home() {
 
                 {/* New Rule Merchants */}
                 <div className="mt-4 flex flex-col justify-center">
+                  <h1 className="mb-4 mt-8 ml-4 text-xl">
+                    Add Merchents for Current Rule:
+                  </h1>
                   <form
                     onSubmit={(event) => {
                       event.preventDefault();
@@ -318,7 +316,7 @@ export default function Home() {
                     }}
                     className="w-full"
                   >
-                    <div className="flex flex-row justify-around">
+                    <div className="flex flex-row justify-around flex-wrap">
                       <input
                         title="name"
                         type="text"
@@ -329,7 +327,7 @@ export default function Home() {
                             event.target.value
                           );
                         }}
-                        placeholder="merchant name"
+                        placeholder=" merchant name"
                         className="border-2 border-stone-400 rounded-md"
                       />
                       <input
@@ -342,64 +340,74 @@ export default function Home() {
                             event.target.value
                           );
                         }}
-                        placeholder="merchant cost"
+                        placeholder=" merchant cost"
                         className="border-2 border-stone-400 rounded-md"
                       />
                       <div className="flex flex-row justify-around">
                         <button
-                          className="py-1 px-2 rounded-md bg-emerald-400"
+                          className="py-1 px-2 rounded-md bg-orange-500"
                           type="submit"
                         >
-                          Add Merchant
+                          <h1 className="text-lg shadow-md text-stone-50">
+                            Add Merchant
+                          </h1>
                         </button>
                       </div>
                     </div>
                   </form>
-                  <div className="m-6">
-                    <h1>Merchants for current rule:</h1>
+                  <h1 className="mt-8 ml-4 text-xl">
+                    Merchants for current rule:
+                  </h1>
+                  <div className="mt-4 flex flex-row justify-center">
+                    <div className="w-9/12">
+                      <ul>
+                        {Array.from(newRule.merchants.entries()).map(
+                          ([merchantName, merchantCost]) => (
+                            <RuleMerchantRow
+                              name={merchantName}
+                              cost={merchantCost}
+                              deleteRuleMerchantRow={
+                                handleDeleteRuleMerchantRow
+                              }
+                            />
+                          )
+                        )}
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <h1 className="mt-8 text-3xl">Rules:</h1>
+                <div className="mt-4 flex flex-row justify-center">
+                  <div className="w-9/12">
                     <ul>
-                      {Array.from(newRule.merchants.entries()).map(
-                        ([merchantName, merchantCost]) => (
-                          <RuleMerchantRow
-                            name={merchantName}
-                            cost={merchantCost}
-                            deleteRuleMerchantRow={handleDeleteRuleMerchantRow}
-                          />
-                        )
-                      )}
+                      {ruleList.map((rule) => (
+                        <RuleRow
+                          key={rule.name}
+                          name={rule.name}
+                          points={rule.points}
+                          merchants={rule.merchants}
+                          deleteRule={handleDeleteRule}
+                        />
+                      ))}
                     </ul>
                   </div>
                 </div>
-                <div className="mt-4">
-                  <h1 className="text-2xl">Rules:</h1>
-                  <ul>
-                    {ruleList.map((rule) => (
-                      <RuleRow
-                        key={rule.name}
-                        name={rule.name}
-                        points={rule.points}
-                        merchants={Array.from(rule.merchants.entries()).map(
-                          ([name, cost]) => ` name: ${name} and cost:${cost}`
-                        )}
-                        deleteRule={handleDeleteRule}
-                      />
-                    ))}
-                  </ul>
-                </div>
               </div>
             </div>
-            {/* Maximum Rewards Button */}
-            <div className="flex flex-row justify-center mt-8">
-              <button
-                className="py-1 px-2 rounded-md bg-emerald-400"
-                onClick={() => calculateMaximumRewards()}
-              >
-                CALCULATE REWARDS
-              </button>
-            </div>
+            <div className="m-4" />
             {/* Maximum Rewards Response */}
             <div>
-              <h1>Maximum Rewards</h1>
+              <h1 className="text-3xl">Maximum Rewards</h1>
+              {/* Maximum Rewards Button */}
+              <div className="flex flex-row justify-center mt-8">
+                <button
+                  className="py-1 px-2 rounded-md shadow-md bg-emerald-400"
+                  onClick={() => calculateMaximumRewards()}
+                >
+                  <h1 className="text-2xl text-stone-50">CALCULATE REWARDS</h1>
+                </button>
+              </div>
+              <div className="m-8" />
               <div>
                 <ul>
                   {transactionGroups ? (
@@ -417,6 +425,7 @@ export default function Home() {
                 </ul>
               </div>
             </div>
+            <div className="m-0 sm:m-4 md:m-8" />
           </div>
         </div>
       </div>
